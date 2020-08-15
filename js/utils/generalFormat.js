fada1316ec07338aadf16b21e6276f1e4cd23c83
Dario Di Gulio <ddigulio@technisys.com>
2020-08-15 13:43:07 -0300

    moment - 15-08-2020

--- js/utils/generalFormat.js
+++ js/utils/generalFormat.js
@@ -1,7 +1,22 @@
 /* Date */
 
-const getShortDate = date => moment(new Date(date)).format('DD/MM/YYYY');
-const getFullCurrentDate = () => moment(new Date()).format('dd:mm:yyyy LLL')
+const SHORT_DATE = date => {
+    moment(date).isValid() ? date = moment(date).format('MM/DD/YYYY') : date = 'Invalid date'
+    return date
+}
+
+const SHORT_CURRENT_DATE = () => {
+    return moment(new Date()).format('MM/DD/YYYY')
+}
+
+const FULL_DATE = date => {
+    moment(date).isValid() ? date = moment(date).format('MM/DD/YYYY LT') : date = 'Invalid date'
+    return date
+}
+
+const FULL_CURRENT_DATE = () => {
+    return moment(new Date()).format('MM/DD/YYYY LT')
+}
 
 
 /* Parse */
@@ -21,8 +36,10 @@ const PESOS = value =>
     });
 
 export default {
-    getShortDate,
-    getFullCurrentDate,
+    SHORT_DATE,
+    SHORT_CURRENT_DATE,
+    FULL_DATE,
+    FULL_CURRENT_DATE,
     DOLAR,
     PESOS
 }
