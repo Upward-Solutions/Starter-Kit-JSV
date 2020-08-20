df01fab7bf995a4dafa18426763c155150b8ae19
DarioDiGulio <39631247+DarioDiGulio@users.noreply.github.com>
2020-08-20 19:04:53 -0300

    fix async

--- js/views/swapi.js
+++ js/views/swapi.js
@@ -1,12 +1,12 @@
 import controllers from '../controllers/index.js'
 import { SUCCES_CODE, showNotification } from '../utils/constants.js'
 
-const getExampleSwapi = async () => {
-    let response = await controllers.exampleSwapi()
-    showNotification(response.message, response.code)
+const getExampleSwapi = () => {
+    controllers.exampleSwapi()
 }
 
 export const setSwapiView = (data) => {
+    showNotification(data.message, data.code)
     if (data.code === SUCCES_CODE) {
         console.log(data);
     }
